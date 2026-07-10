46a878e221a8e8af84e2e2dddb68e03b8882a313
Praniket Walavalkar <praniketkw@gmail.com>
2026-07-09 17:54:42 -0700

    Add: min-cost-climbing-stairs - submission-14

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-14.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = {len(cost):0}
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if i>=len(cost):
+                return 0
+            
+            dp[i] = cost[i]+ min(dfs(i+1), dfs(i+2))
+            return dp[i]
+        
+        return min(dfs(0), dfs(1))
