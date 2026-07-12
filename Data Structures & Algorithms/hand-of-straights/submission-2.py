085eaee833010f55107cb6667134b2b4ef880a93
Praniket Walavalkar <praniketkw@gmail.com>
2026-07-11 22:30:51 -0700

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand)%groupSize!=0:
+            return False
+        
+        count = Counter(hand)
+        minheap = []
+
+        for n,c in count.items():
+            heapq.heappush(minheap, n)
+        
+        while minheap:
+            minimum = minheap[0]
+            for i in range(minimum,minimum+groupSize):
+                if count[i]==0 or i not in count:
+                    return False
+                count[i]-=1
+            while minheap and count[minheap[0]]==0:
+                heapq.heappop(minheap)
+        
+        return True
+
+
